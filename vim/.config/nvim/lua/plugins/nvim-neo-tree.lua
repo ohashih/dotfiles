31bb51d390ae52cc64938de202c5bdbd8866f44f
kurage <ohashih@ns.aucnet.co.jp>
2024-10-29 00:46:23 +0900

    chore: add indent higtlight

--- vim/.config/nvim/lua/plugins/nvim-neo-tree.lua
+++ vim/.config/nvim/lua/plugins/nvim-neo-tree.lua
@@ -14,7 +14,7 @@ return {
           [vim.diagnostic.severity.ERROR] = " ",
           [vim.diagnostic.severity.WARN] = " ",
           [vim.diagnostic.severity.INFO] = " ",
-          [vim.diagnostic.severity.HINT] = "󰌵",
+          -- [vim.diagnostic.severity.HINT] = "󰌵",
         },
       },
     }),
--- vim/.config/nvim/lua/plugins/ui.lua
+++ vim/.config/nvim/lua/plugins/ui.lua
@@ -6,36 +6,56 @@ return {
     priority = 1000,
   },
   -- hightlight
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = "UIEnter",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
-  opts = {
-    highlight = { enable = true },
-    auto_install = false,
-    ensure_installed = "all",
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
-          ["ab"] = "@block.outer",
-          ["ib"] = "@block.inner",
-          ["al"] = "@call.outer",
-          ["il"] = "@call.inner",
-          ["aP"] = "@parameter.outer",
-          ["iP"] = "@parameter.inner",
-          ["ao"] = "@conditional.outer",
-          ["io"] = "@conditional.inner",
-          ["as"] = "@statement.outer",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = "UIEnter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    opts = {
+      highlight = { enable = true },
+      auto_install = false,
+      ensure_installed = "all",
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["ab"] = "@block.outer",
+            ["ib"] = "@block.inner",
+            ["al"] = "@call.outer",
+            ["il"] = "@call.inner",
+            ["aP"] = "@parameter.outer",
+            ["iP"] = "@parameter.inner",
+            ["ao"] = "@conditional.outer",
+            ["io"] = "@conditional.inner",
+            ["as"] = "@statement.outer",
+          },
         },
       },
     },
+  },
+  {
+    "shellRaining/hlchunk.nvim",
+    lazy = true,
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("hlchunk").setup({
+        chunk = {
+          enable = true,
+          use_treesitter = true,
+          notify = true,
+        },
+        indent = {
+          enable = true,
+          chars = { "│" },
+        },
+      })
+    end,
   },
 }
