76ac35db94dc84c792eae33c9758faabadf63063
kurage <ohashih@ns.aucnet.co.jp>
2024-10-31 23:14:00 +0900

    chore: Change keymaps

--- vim/.config/nvim/lua/core/keymaps.lua
+++ vim/.config/nvim/lua/core/keymaps.lua
@@ -123,14 +123,10 @@ keymap("n", "<leader>me", ":NoiceErrors<CR>", ex_opts("Noice error"))
 keymap("n", "<leader>df", ":DiffviewOpen<CR>", ex_opts("DiffviewOpen"))
 keymap("n", "<leader>dc", ":DiffviewClose<CR>", ex_opts("DiffviewClose"))
 keymap("n", "<leader>fv", ":DiffviewFileHistory<CR>", ex_opts("DiffviewFileHistory"))
+keymap("n", "gb", ":GitBlameToggle<CR>", opts)
 
 -- NeoTree
 keymap("n", "<C-n>", ":Neotree toggle<CR>", opts)
-
--- DiffviewOpen
-
-keymap("n", "<leader>g", ":DiffviewOpen<CR>", opts)
-keymap("n", "gb", ":GitBlameToggle<CR>", opts)
 
 -- lsp
 vim.api.nvim_create_autocmd("LspAttach", {
