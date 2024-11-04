f6ea3515ed5367c47ae930c5904a6890f80f1091
kurage <ohashih@ns.aucnet.co.jp>
2024-11-05 08:37:27 +0900

    chore: autocmd

--- vim/.config/nvim/lua/core/autocmd.lua
+++ vim/.config/nvim/lua/core/autocmd.lua
@@ -1,7 +1,8 @@
 local vim = vim
 
 -- ファイル保存時に自動フォーマット
-vim.api.nvim_create_augroup("LspFormatOnSave", { clear = true })
+
+--[[ vim.api.nvim_create_augroup("LspFormatOnSave", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePre", {
   group = "LspFormatOnSave",
   pattern = "*",
@@ -15,4 +16,4 @@ vim.api.nvim_create_autocmd("BufWritePre", {
       bufnr = vim.api.nvim_get_current_buf(),
     })
   end,
-})
+}) ]]
