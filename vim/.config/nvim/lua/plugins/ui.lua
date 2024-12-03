aa2b38d627297d13c1c03a0c2baf9283bd9a229d
kurage <ohashih@ns.aucnet.co.jp>
2024-12-03 17:00:14 +0900

    feat: Add fold function

--- vim/.config/nvim/lua/plugins/ui.lua
+++ vim/.config/nvim/lua/plugins/ui.lua
@@ -46,13 +46,9 @@ return {
   -- yank highlight
   {
     "machakann/vim-highlightedyank",
-    lazy = true,            -- 遅延読み込みを有効に
     event = "TextYankPost", -- ヤンクイベントが発生したときに読み込む
     config = function()
-      -- ハイライトの設定
       vim.g.highlightedyank_highlight_duration = 300 -- ハイライトが表示される時間（ミリ秒）
-
-      -- ヤンク後のハイライト
       vim.api.nvim_exec(
         [[
         augroup highlight_yank
@@ -72,7 +68,65 @@ return {
       require("neoscroll").setup()
     end,
   },
-  -- auto-pairs
+  -- scroll view
+  {
+    "dstein64/nvim-scrollview",
+    lazy = true,
+  },
+  -- hightlight
+  {
+    "dominikduda/vim_current_word",
+    lazy = false,
+    config = function()
+      vim.g["current_word#highlight_delay"] = 100
+      vim.g["current_word#highlight_twins"] = 1
+      vim.g["current_word#enabled"] = 1
+      vim.cmd([[highlight CurrentWord guibg=#45475a guifg=NONE ctermbg=238]])
+      vim.cmd([[highlight CurrentWordTwins guibg=#585b70 guifg=NONE ctermbg=240]])
+    end,
+  },
+  --fold
+  {
+    "anuvyklack/pretty-fold.nvim",
+    lazy = true, -- 必要に応じて遅延読み込み
+    event = { "BufReadPost", "BufNewFile" }, -- ファイル読み込み時に読み込む
+    config = function()
+      require("pretty-fold").setup({
+        sections = {
+          left = {
+            "content", -- フォールド内のテキストを表示
+          },
+          right = {
+            " ", "number_of_folded_lines", " lines ", -- 折り畳まれた行数を表示
+          },
+        },
+        fill_char = "·", -- フォールドラインを埋める文字
+        remove_fold_markers = true, -- デフォルトのフォールドマーカーを非表示
+        -- カスタマイズ可能なキーマッピング
+        key_mappings = {
+          toggle_fold = "za", -- フォールドの開閉
+          close_all_folds = "zM", -- 全てのフォールドを閉じる
+          open_all_folds = "zR", -- 全てのフォールドを開く
+        },
+      })
+    end,
+  },
+  --fold-preview
+  {
+    "anuvyklack/fold-preview.nvim",
+    dependencies = { "anuvyklack/keymap-amend.nvim" }, -- 必須依存プラグイン
+    lazy = true, -- 遅延読み込みを有効化
+    event = { "BufReadPost", "BufNewFile" }, -- ファイル読み込み時に読み込む
+    config = function()
+      require("fold-preview").setup({
+        border = "rounded", -- プレビューウィンドウのボーダータイプ ("none", "single", "double", "rounded", "shadow")
+        auto = 500, -- 自動プレビューまでの遅延時間（ms）
+        preview_command = "bat --style=plain --paging=never", -- プレビュー内容を整形するコマンド
+        default_keymaps = true, -- デフォルトのキーマッピングを有効にする
+      })
+    end,
+  },
+ -- auto-pairs
   --[[ {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
